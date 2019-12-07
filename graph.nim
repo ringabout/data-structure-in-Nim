a62c59088d787e726e7811d38ed0e9c883380d08
flywind <xzsflywind@gmail.com>
2019-12-07 11:50:39 +0800

    update

--- graph.nim
+++ graph.nim
@@ -5,8 +5,12 @@ type
   Graph* = object
     data: seq[seq[int]]
     vertex, edge: int
+  DepthFirstSearch* = object
+    graph: Graph
+    marked*: seq[bool]
+    count*: int
 
-proc addEdge*(g:var Graph; v, w: int)
+proc addEdge*(g: var Graph; v, w: int)
 
 proc initGraph*(vertex: int): Graph =
   result.vertex = vertex
@@ -14,7 +18,7 @@ proc initGraph*(vertex: int): Graph =
 
 proc initGraph*(file: File): Graph =
   var strm = newFileStream(file)
-  let 
+  let
     vertex = parseInt(strm.readLine)
     edge = parseInt(strm.readLine)
   result = initGraph(vertex)
@@ -23,45 +27,40 @@ proc initGraph*(file: File): Graph =
       line = strm.readLine.split
       (v, w) = (line[0].parseInt, line[1].parseInt)
     result.addEdge(v, w)
-  # var line = ""
-  # while readLine(file, line):
 
-
-proc addEdge*(g:var Graph; v, w: int) = 
+proc addEdge*(g: var Graph; v, w: int) =
   g.data[v].add(w)
   g.data[w].add(v)
   g.edge += 1
 
-
-iterator items*(g: Graph, vertex: int): int =
+iterator items*(g: Graph; vertex: int): int =
   for item in g.data[vertex]:
     yield item
-
 
 proc vertex*(g: Graph): int =
   g.vertex
 
-proc edge*(g: Graph): int = 
+proc edge*(g: Graph): int =
   g.edge
 
-proc degree*(g: Graph, vertex: int): int = 
+proc degree*(g: Graph; vertex: int): int =
   g.data[vertex].len
 
-proc maxDegree*(g: Graph): int = 
+proc maxDegree*(g: Graph): int =
   result = g.data[0].len
   for idx in 1 ..< g.vertex:
     result = max(result, g.data[idx].len)
 
-proc avgDegree*(g: Graph): float = 
+proc avgDegree*(g: Graph): float =
   2.0 * (g.edge / g.vertex)
 
-proc numOfSelfLoops*(g: Graph): int = 
+proc numOfSelfLoops*(g: Graph): int =
   for v in 0 ..< g.vertex:
     for w in g.items(v):
       if v == w: result += 1
   result div 2
 
-proc `$`*(g: Graph): string = 
+proc `$`*(g: Graph): string =
   result.add &"{g.vertex} vertices, {g.edge} edges\n"
   for v in 0 ..< g.vertex:
     result.add &"{v}: "
@@ -69,7 +68,26 @@ proc `$`*(g: Graph): string =
       result.add &"{w} "
     result.add "\n"
 
+proc dfs*(g: var DepthFirstSearch; v: int) =
+  g.marked[v] = true
+  g.count += 1
+  for w in g.graph.items(v):
+    if not g.marked[w]: dfs(g, w)
+
+proc marked*(g: DepthFirstSearch; w: int): bool =
+  g.marked[w]
+
+proc count*(g: DepthFirstSearch): int =
+  g.count
+
+proc initDepthFirstSearch*(g: Graph; s: int): DepthFirstSearch =
+  result.graph = g
+  result.marked = newSeq[bool](g.vertex)
+  dfs(result, s)
+
+
+
 
 var f = open("graph.txt", fmread)
 var g = initGraph(f)
-echo g
+echo initDepthFirstSearch(g, 3)
--- matrix.nim
+++ matrix.nim
@@ -6,18 +6,43 @@ type
     data: seq[seq[T]]
     rows: int
     cols: int
-  CMatrix*[T] = object
+  CMatrix*[T] {.shallow.}= object
     data: seq[T]
     rows: int
     cols: int
+  Matrix*[T] = CMatrix[T]
 
-proc newRMatrix*[T](rows, cols: int): RMatrix[T] {.noinit.} =
+
+template checkIndex*(cond: untyped, msg = "") =
+  when compileOption("boundChecks"):
+    {.line.}:
+      if not cond:
+        raise newException(IndexError, msg)
+
+proc newRMatrix*[T](rows, cols: Positive): RMatrix[T] {.noinit.} =
   result.rows = rows
   result.cols = cols
   result.data = newSeqWith(cols, newSeqUninitialized[T](rows))
 
-proc newCMatrix*[T](rows, cols: int): CMatrix[T] {.noinit.} =
+proc newMatrix*[T](rows, cols: Positive): Matrix[T] {.noinit.} =
   result.rows = rows
   result.cols = cols
   result.data = newSeqUninitialized[T](rows * cols)
 
+
+# proc mapIndex[T](m: seq[T], row, col: Positive): T =
+#   for row in 0 ..< rows:
+#     for col in 0 ..< cols:
+#       result.data[row * cols + col] =
+
+proc newMatrix*[T](x: sink seq[T]; rows, cols: Positive): Matrix[T] =
+  checkIndex(rows * cols == x.len)
+  result.rows = rows
+  result.cols = cols
+  result.data = x
+  
+proc newMatrix*[T](x: sink Matrix[T]): Matrix[T] {.noinit.} =
+  checkIndex(rows * cols == x.len)
+  result = x
+
+echo newMatrix(@[1, 2, 3, 4], 2, 2)
